b07b6c19be79a4f9a5a4259a0b3df37f0193baed
abdiye <bentaabdi@gmail.com>
2022-07-15 07:10:46 -0700

    commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+  *print_list - prints all the elements of a list_t.
+  *@h: pointer to list.
+  *
+  *Return: the number of nodes.
+  */
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	count = 0;
