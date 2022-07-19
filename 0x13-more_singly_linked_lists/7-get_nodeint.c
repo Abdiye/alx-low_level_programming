24fb6e4ee78be6506cfde825edc3fb0d939cac1a
abdiye <bentaabdi@gmail.com>
2022-07-19 11:32:33 -0700

    commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -40,5 +40,22 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (count <= index)
 	{
 		return (NULL);
-	} 
+	}
+	else
+	{
+		count = 0;
+		while (current != NULL)
+		{
+			if (count == index)
+			{
+				break;
+			}
+			else
+			{
+				count++;
+				current = current->next;
+			}
+		}
+		return (current);
+	}
 }
